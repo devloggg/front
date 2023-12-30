400b9dd44ea045dce614b3a3f7425669c6404a9b
devhoodit <hoodit@naver.com>
2023-12-31 06:50:03 +0900

    next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path')
- 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+
+const nextConfig = {
+    output: 'export',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
 }
+ 
+module.exports = nextConfig
