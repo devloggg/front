b9a43a320bf6a938dd01b138945eeedd863073f0
devhoodit <hoodit@naver.com>
2024-01-01 09:15:59 +0900

    page: article page

---
+++ app/article/page.tsx
@@ -0,0 +1,9 @@
+import Article from './article'
+
+export default function Page() {
+  return (
+    <div className="main__section">
+      <Article url="https://raw.githubusercontent.com/facebook/react/main/README.md" />
+    </div>
+  )
+}
