d0ae3b189bb58db7d974ed6609f5f89a08ad974e
devhoodit <hoodit@naver.com>
2024-01-01 09:16:38 +0900

    root layout

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Devlog',
+  description: 'Devlog, log your dev life',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
