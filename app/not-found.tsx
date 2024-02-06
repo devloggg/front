0c60dd290451d1c04d3af6abf89d71389ba25d61
devhoodit <hoodit@naver.com>
2024-02-06 22:19:27 +0900

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,17 @@
+import Footer from '@/components/footer'
+import Header from '@/components/header'
 import '@/styles/NotFound.sass'
 
 export default function NotFound() {
   return (
-    <div className="not-found">
-      <h2>Not Found</h2>
-    </div>
+    <html lang="ko">
+      <body>
+        <Header />
+        <div className="not-found">
+          <h2>Not Found</h2>
+        </div>
+        <Footer />
+      </body>
+    </html>
   )
 }
