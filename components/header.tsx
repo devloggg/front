0accb86edc8517ac6da17fe3bd9a12c374de8dd7
devhoodit <hoodit@naver.com>
2024-02-06 22:21:03 +0900

    header

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import '../styles/Header.sass'
+import '@/styles/Header.sass'
 
 export default function Header(): React.ReactNode {
   return (
     <header>
       <div className="title">
-        <h1>Dev</h1>
+        <h1>Devlog</h1>
       </div>
-      <div className="bar" />
     </header>
   )
 }
